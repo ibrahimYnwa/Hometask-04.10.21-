4b35af811b3a9f27346411a3ba3830bf9bfe2afe
ibrahim <ibrahimea@code.edu.az>
2021-10-04 22:01:35 +0400

    Hometask

---
+++ homework/New Hometask/New Hometask/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace New_Hometask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //int result = oddnumberssum(0, 9);
+
+            //Console.WriteLine(result);
+            #endregion
+
+            #region
+            //int input = 40;
+
+            //if (input%7==0)
+            //{
+            //    Console.WriteLine("bolunur");
+            //}
+            //else
+            //{
+            //    int qaliq = input % 7;
+
+            //    if (qaliq>3)
+            //    {
+            //        while (input % 7 !=0)
+            //        {
+            //            input++;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        while (input%7 !=0)
+            //        {
+            //            input--; 
+
+            //        }
+
+            //    }
+            //    Console.WriteLine(input);
+            #endregion
+        }
+    }
+
+
+
+        #region       //public static int oddnumberssum (int number1 ,int number2)
+
+        //{
+        //    int result = 0; 
+
+        //    for (int i = number1; i <= number2; i++)
+        //    {
+        //        if (i%2==0)
+        //        {
+        //            result += i;
+
+        //        }
+
+
+        //    }
+        //    return result;
+        #endregion
+    }
+
+
+
